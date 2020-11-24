8538319d135a69b496ba1ba8ff1f86c68bfa665b
Bruno Pedrazza <pedrazzabruno@gmail.com>
2020-11-24 20:27:16 -0300

    Change results path to be generic

--- configsettings.py
+++ configsettings.py
@@ -1,10 +1,11 @@
 import sys
 
+
 class Configs:
     def __init__(self):
         downloads_path = ''
         platform = ''
-        self.results_path = 'C:/Users/Bruno/Code/veganti-delivery/results/'
+        self.results_path = '/results'
         if sys.platform == 'win32':
             downloads_path = 'C:/Users/Bruno/Downloads/'
             platform = 'windows'
