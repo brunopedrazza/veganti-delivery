2db51004279c45dca150ce542facb019c1e46d61
Bruno Pedrazza <bruno.pedrazza@transferoswiss.ch>
2020-12-11 21:39:49 -0300

    Remove old files from project root

--- holiday.py
+++
@@ -1,9 +0,0 @@
-class Holiday:
-    def __init__(self, date, name, type, type_code):
-        self.date = date
-        self.name = name
-        self.type = type
-        self.type_code = type_code
-
-    def __str__(self):
-        return f'Data: {self.date}\tNome: {self.name}\tTipo: {self.type}'
